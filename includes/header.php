f7459fdd52f920e67a59b15542ae86108687ec52
wesleyanjosz <wesleyanjoszero@gmail.com>
2025-11-04 13:45:04 -0300

    update - adicioando include com header footer

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+ <html lang="pt-br">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>CRUD</title>
+     <!--Import Google Icon Font-->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+     <!--Let browser know website is optimized for mobile-->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ </head>
+
+ <body>
