a1ff67749b6e022f2d3a70412d10f0dc73591e15
wesleyanjosz <wesleyanjoszero@gmail.com>
2025-11-04 13:53:05 -0300

    update - adicionando tabela cliente estatica

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php
-
 include_once './includes/header.php';
+?>
+
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3 class="light">Clientes</h3>
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Nome:</th>
+                    <th>Sobrenome:</th>
+                    <th>E-mail:</th>
+                    <th>Idade:</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Wesley</td>
+                    <td>Anjos</td>
+                    <td>wesley@gmail.com</td>
+                    <td>28</td>
+                    <td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                    <td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <a href="" class="btn">Adicionar cliente</a>
+    </div>
+</div>
+
+<?php
 include_once './includes/footer.php';
+?>
